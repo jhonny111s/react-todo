99ced99d13dd5658bd974a15d6f5937f42c9308f
jhony.lopez <jhonny111s@gmail.com>
2018-05-08 18:53:41 -0500

    se agrego estado de lista de todo y función para agregar un item a la lista

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,41 @@ import AppTodo from './containers/AddTodo';
 import MenuFilter from './containers/MenuFilter';
 import VisibleTodoList from './containers/VisibleTodoList';
 
-class App extends React.Component {
+
+
+class App extends React.Component<any, any> {
+
+  constructor(props: any) {
+    super(props);
+    this.addItemHandler = this.addItemHandler.bind(this);
+    this.state = {
+      todo: ['item 1', 'item 2', 'item 3'], 
+    };
+  }
+
+
+  public addItemHandler = (newItem: string) => {
+    console.log(newItem);
+    if(!newItem.trim()) {
+      return;
+      
+    }
+
+    const newTodo: string[] = this.state.todo.concat(newItem);
+
+    if (this.state.todo !== newTodo ) {
+      this.setState({todo: newTodo});
+    }
+    
+  }
+
+
   public render() {
     return (
       <div className="App">
         <div className="App-intro">
-          <AppTodo/>
-          <VisibleTodoList/>
+          <AppTodo add= {this.addItemHandler}/>
+          <VisibleTodoList todo = {this.state.todo}/>
           <MenuFilter/>
         </div>   
       </div>
