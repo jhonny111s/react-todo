1b2e499be656c26d33c359834bf6ee88f2d0b52e
jhony.lopez <jhonny111s@gmail.com>
2018-05-10 13:06:28 -0500

    se agrego función para saber cuando dan click en un filtro e informarlo al padre

--- src/containers/MenuFilter.tsx
+++ src/containers/MenuFilter.tsx
@@ -1,18 +1,28 @@
 // debe ser asi para que se reconozcan los types
 import * as React from 'react';
+import Link from '../components/Link'
 
 
 // compont tiene dos parametros props y states, son objetos
-class MenuFilter extends React.Component <{},{}> {
+class MenuFilter extends React.Component <any,any> {
 
+  constructor(props: any) {
+    super(props);
+    this.filterChange = this.filterChange.bind(this);
+  }
+
+
+  public filterChange = (event:any) => {
+    this.props.change(event.target.id);
+  }
 
   public render() {
     return (
         <div>
             <p>Menu filter</p>
-            <button> All </button>
-            <button> Active </button>
-            <button> Complete </button>
+            <Link id={"SHOW_ALL"} active = {this.props.filter === 'SHOW_ALL'} click={this.filterChange}> All </Link>
+            <Link id= {"SHOW_ACTIVE"} active = {this.props.filter === 'SHOW_ACTIVE'} click={this.filterChange}> Active </Link>
+            <Link id= {"SHOW_COMPLETED"} active = {this.props.filter === 'SHOW_COMPLETED'} click={this.filterChange}> Complete </Link>
         </div>
     );
   }
