7a05e79c5ee28ec2a0129492d37feb595aafb84a
jhony.lopez <jhonny111s@gmail.com>
2018-05-10 11:51:40 -0500

    se elimino todo por defecto y se organizaron funciones para agregar item y el toggle de un item

--- src/App.tsx
+++ src/App.tsx
@@ -13,25 +13,37 @@ class App extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
     this.addItemHandler = this.addItemHandler.bind(this);
+    this.toogleItemHandler = this.toogleItemHandler.bind(this);
     this.state = {
-      todo: ['item 1', 'item 2', 'item 3'], 
+      todo: [
+   
+      ], 
     };
   }
 
 
-  public addItemHandler = (newItem: string) => {
-    console.log(newItem);
-    if(!newItem.trim()) {
+  public addItemHandler (newItem: any): any {
+    if(!newItem.text.trim()) {
       return;
-      
     }
 
-    const newTodo: string[] = this.state.todo.concat(newItem);
-
+    const newTodo = this.state.todo.concat(newItem);
     if (this.state.todo !== newTodo ) {
       this.setState({todo: newTodo});
     }
     
+  }
+
+  public toogleItemHandler (id: string): any {
+    
+    const newTodo = this.state.todo.map((item: any):any => {
+      if (item.id === id) {
+        item.completed = !item.completed;
+      }
+      return item;
+    });
+
+    this.setState({todo: newTodo}); 
   }
 
 
@@ -40,7 +52,7 @@ class App extends React.Component<any, any> {
       <div className="App">
         <div className="App-intro">
           <AppTodo add= {this.addItemHandler}/>
-          <VisibleTodoList todo = {this.state.todo}/>
+          <VisibleTodoList todo = {this.state.todo} toogle= {this.toogleItemHandler}/>
           <MenuFilter/>
         </div>   
       </div>
